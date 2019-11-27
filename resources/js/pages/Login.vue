4ddf746478336b28ad0b17a2b3cb29345f08a60c
tnoka <mxx.2963@icloud.com>
2019-11-28 02:01:34 +0900

    add login.vue

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,5 +1,69 @@
 <template>
-<h1>
-Login
-</h1>
+    <div class="container--small">
+        <ul class="tab">
+            <li
+                class="tab__item"
+                :class="{'tab__item--active': tab === 1 }"
+                @click="tab = 1"
+            >ログイン</li>
+            <li class="tab__item"
+                :class="{'tab__item--active': tab === 2 }"
+                @click="tab = 2"
+            >新規登録</li>
+        </ul>
+        <div class="panel" v-show="tab === 1">
+            <form class="form" @submit.prevent="login">
+                <label for="login-email">E-Mail</label>
+                <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+                <label for="login-password">Password</label>
+                <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+                <div class="form__button">
+                    <button type="submit" class="button button-inverse">Login</button>
+                </div>
+            </form>
+        </div>
+        <div class="panel" v-show="tab === 2">
+            <form class="form" @submit.prevent="register">
+                <label for="username">Name</label>
+                <input type="text" class="form__item" id="username" v-model="registerForm.username">
+                <label for="email">E-Mail</label>
+                <input type="text" class="form__item" id="email" v-model="registerForm.email">
+                <label for="password">Password</label>
+                <input type="password" class="form__item" id="password" v-model="registerForm.password">
+                <label for="password-confirmation">password（再入力）</label>
+                <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+                <div class="form__button">
+                    <button type="submit" class="button button--inverse">登録</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            tab: 1,
+            loginForm: {
+                email: '',
+                password: '',
+            },
+            registerForm: {
+                username: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
+            }
+        }
+    },
+    methods: {
+        login() {
+            console.log(this.loginForm)
+        },
+        register() {
+            console.log(this.registerForm)
+        },
+    }
+}
+</script>
