42a7ca015129ac302ea89389e8240b2143c982aa
tnoka <mxx.2963@icloud.com>
2019-11-28 21:54:58 +0900

    add CSRF

---
+++ resources/js/util.js
@@ -0,0 +1,17 @@
+// クッキーの値を取得
+export function getCookieValue(searchKey) {
+    if (typeof searchKey === 'undefined') {
+        return ''
+    }
+
+    let val = ''
+
+    document.cookie.split(';').forEach(cookie => {
+        const [key, value] = cookie.split('=')
+        if (key === searchKey) {
+            return val = value
+        }
+    })
+
+    return val
+}
