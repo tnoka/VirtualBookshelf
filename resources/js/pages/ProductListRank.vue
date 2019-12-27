fd3ad29af03df6ca75eba4217605c3615c967c13
tnoka <mxx.2963@icloud.com>
2019-12-28 03:41:30 +0900

    読み込み演出の時間を変更

--- resources/js/pages/ProductListRank.vue
+++ resources/js/pages/ProductListRank.vue
@@ -24,7 +24,7 @@
             </div>
 
             <div class="container" v-else>
-                <div  class="jumbotron jumbotron-extend home-header"  style="background: url(../img/main.jpg) no-repeat center center; background-size: cover;">
+                <div  class="jumbotron jumbotron-extend home-header" style="background: url(../img/main.jpg) no-repeat center center; background-size: cover;">
                     <div class="container-fluid jumbotron-container">
                         <h1 class="display-4 site-name text-light text-center mt-5 top-title" style="">仮想本棚</h1>
                         <h3 class="site-name text-light text-center mt-5 top-title">読んだ本を本棚に飾り</h3>
@@ -84,7 +84,7 @@ export default {
             setTimeout(function(){
                 self.isLoading = false;
                 console.log('load off');
-            }, 1300);
+            }, 800);
             const response = await axios.get(`/api/products/indexRank/?page=${this.$route.query.page}`)
 
             if(response.status !== OK) {
