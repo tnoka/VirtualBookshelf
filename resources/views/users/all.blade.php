b00d833b071a90e77b6fee96d5139ec59ee304fd
tnoka <mxx.2963@icloud.com>
2019-12-29 00:28:28 +0900

    ユーザー一覧のUIを改善

--- resources/views/users/all.blade.php
+++ resources/views/users/all.blade.php
@@ -9,11 +9,11 @@
             @foreach ($all_users as $user)
                 <div class="card">
                     <div class="card-haeder p-3 w-100 d-flex">
-                        <img src="{{ asset('https://s3-ap-northeast-1.amazonaws.com/virtualbookshelf/' .$user->profile_image) }}" class="rounded-circle" width="50" height="50" alt="">
+                        <a href="{{ url('users/' .$user->id) }}"><img src="{{ asset('https://s3-ap-northeast-1.amazonaws.com/virtualbookshelf/' .$user->profile_image) }}" class="rounded-circle" width="50" height="50"></a>
                         <div class="ml-3 flex-column">
                             <h5><a href="{{ url('users/' .$user->id) }}" class="text-muted font-weight-bold">{{ $user->name }}</a></h5>
                         </div>
-                        <div class="d-flex justify-content-end flex-grow-1">
+                        <div class="d-flex align-items-center justify-content-end flex-grow-1">
                             <form action="{{ url('login') }}"><button type="submit" class="btn btn-secondary"  onclick="return confirm('フォロー機能を使う場合は、ログインしてください');">フォローする</button></form>
                         </div>
                     </div>
