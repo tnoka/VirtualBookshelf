499f312ba82e436bc5c25c2184c6ac10e264008d
tnoka <mxx.2963@icloud.com>
2020-01-10 23:07:19 +0900

    コメントの追加

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('auth/twitter', 'Auth\SocialAuthController@redirectToProvider');
 Route::get('auth/twitter/callback', 'Auth\SocialAuthController@handleProviderCallback');
 Route::get('auth/twitter/logout', 'Auth\SocialAuthController@logout');
+// TwitterAPI取得用
 Route::get('terms',function(){return view('TermsOfService');});
 Route::get('privacy',function(){return view('privacy');});
 
