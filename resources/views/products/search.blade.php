457f6dc1441f1bb13299e30cc5861de9e97cf98a
tnoka <mxx.2963@icloud.com>
2020-01-12 01:29:19 +0900

    addSearch

--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <form class="form-inline">
-                    <div class="form-group">
-                        <input type="text" name="keyword" value="{{ $keyword }}"
+            <div class="col-md-3 mx-auto">
+                <form class="">
+                    <div class="form-group text-center px-5">
+                        <input type="text" class="form-control py-4 text-center" name="keyword" value="{{ $keyword }}"
                         placeholder="タイトルか著者名を入力">
-                        <input type="submit" value="検索">
+                        <input type="submit" class="btn btn-lg btn-dark mt-2 px-5" value="検索">
                     </div>
                 </form>
             </div>
@@ -19,8 +19,8 @@
         @if(count($products) > 0)
             <div class="row">
                 @foreach($products as $product)
-                <div class="col-md-3">
-                    {{ $product->title }}
+                <div class="col-md-4">
+                    <a href="{{ url("products/". $product->id) }}"><img src="{{ asset('https://s3-ap-northeast-1.amazonaws.com/virtualbookshelf/' .$product->product_image) }}" width="300" height="300" class="mt-4 d-block mx-auto img-fluid img-responsive thumbnail aligncenter size-full wp-image-425" style="cursor:pointer" /></a>
                 </div>
                 @endforeach
             </div>
@@ -28,7 +28,7 @@
             <p>ヒットしませんでした。</p>
             @endif
 
-            <div class="paginate">
+            <div class="my-4 d-flex justify-content-center">
                 {{ $products->links() }}
             </div>
     </div>
