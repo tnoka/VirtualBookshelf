c27280070917a7ce17ac843bfa48ddc14d090947
tnoka <mxx.2963@icloud.com>
2020-01-26 22:35:33 +0900

    投稿削除機能のテストを追加

---
+++ tests/Feature/DestroyProductApiTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Product;
+use App\User;
+
+
+class DestroyProductApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        factory(Product::class)->create();
+        $this->product = Product::first();
+
+        $this->user = User::first();
+    }
+    /**
+     * @test
+     */
+    public function DestroyProduct_本を削除することができるか()
+    {
+
+        $response = $this->actingAs($this->user)
+        ->DELETE('products'. '/'. $this->product->id, [
+            'id' => $this->product->id,
+        ]);
+
+        $response->assertStatus(302);
+
+        $products = Product::all();
+
+        $this->assertEquals(0, $products->count());
+
+    }
+}
